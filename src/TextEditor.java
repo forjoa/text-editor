da8b9f832b5afe74303cb0bff5d6f86fcc11368e
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-10-01 18:50:49 +0200

    build: init content panel

--- src/TextEditor.java
+++ src/TextEditor.java
@@ -3,6 +3,13 @@ import java.awt.*;
 
 public class TextEditor extends JFrame {
     public TextEditor() throws HeadlessException {
+        // content panel
+        JPanel contentPane = new JPanel();
+        setContentPane(contentPane);
+        setSize(800, 800);
+        setLocationRelativeTo(null);
 
+
+        setVisible(true);
     }
 }
